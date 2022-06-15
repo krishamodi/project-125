814f702aa71686c9cf4dc9f3bb6a3facab449d1a
krishamodi <95953266+krishamodi@users.noreply.github.com>
2022-06-15 17:43:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+difference=0;
+rightWristX=0;
+leftWristY=0;
+
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+
+canvas=createCanvas(550,550);
+canvas.position(560,150);
+
+poseNet=ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log('PoseNet is intialized!');
+}
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+        difference=floor(leftWristX-rightWristX);
+    }
+}
+
+var name="Teddy";
+
+function draw(){
+    background("#ee72f2");
+
+    document.getElementById("font_size").innerHTML="Font size of the text will be: "+ difference+"px";
+    textSize(difference);
+
+text(name,50,400);
+}
